8613417647c2143f73151e3e636c8dc78fb20aaa
can gündoğdu <gundogdu.can@hotmail.com>
2024-11-25 23:07:57 +0300

    25.11.2024 #lesson16

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
 
 namespace TravelTripProject.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: Blog
+        
+        Context c= new Context();
         public ActionResult Index()
         {
-            return View();
+            var bloglar=c.Blogs.ToList();
+            return View(bloglar);
+        }
+
+        public ActionResult BlogDetay(int id)
+        {
+            var blogbul=c.Blogs.Where(x=>x.ID==id).ToList();
+            return View(blogbul);
         }
     }
 }

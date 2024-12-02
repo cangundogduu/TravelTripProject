6acc40dabbbbc44791c1c8c3a1a64eb22d8af471
can gündoğdu <gundogdu.can@hotmail.com>
2024-12-02 23:14:00 +0300

    02.12.2024#lesson49 final

--- TravelTripProject/Controllers/AdminController.cs
+++ TravelTripProject/Controllers/AdminController.cs
@@ -10,6 +10,8 @@ namespace TravelTripProject.Controllers
     public class AdminController : Controller
     {
         Context c = new Context();
+
+        [Authorize]
         public ActionResult Index()
         {
             var deger = c.Blogs.ToList();
@@ -32,7 +34,7 @@ namespace TravelTripProject.Controllers
 
         public ActionResult BlogSil(int id)
         {
-            var value= c.Blogs.Find(id);
+            var value = c.Blogs.Find(id);
             c.Blogs.Remove(value);
             c.SaveChanges();
             return RedirectToAction("Index");
@@ -42,24 +44,61 @@ namespace TravelTripProject.Controllers
 
         public ActionResult BlogGetir(int id)
         {
-            var bl= c.Blogs.Find(id);
-            return View("BlogGetir",bl);
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir", bl);
         }
-    
-    
-    public ActionResult BlogGuncelle(Blog blog)
+
+
+        public ActionResult BlogGuncelle(Blog blog)
         {
             var blg = c.Blogs.Find(blog.ID);
-            blg.Baslik=blog.Baslik;
-            blg.Aciklama=blog.Aciklama;
-            blg.BlogImage=blog.BlogImage;
-            blg.Tarih=blog.Tarih;
+            blg.Baslik = blog.Baslik;
+            blg.Aciklama = blog.Aciklama;
+            blg.BlogImage = blog.BlogImage;
+            blg.Tarih = blog.Tarih;
             c.SaveChanges();
             return RedirectToAction("Index");
-                
-                
-                
-       }
-    
+
+
+
+        }
+        [Authorize]
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+
+
+        public ActionResult YorumSil(int id)
+        {
+            var value = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(value);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi", "Admin");
+
+        }
+
+        [HttpGet]
+        public ActionResult YorumGuncelle(int id)
+        {
+            var deger = c.Yorumlars.Find(id);
+            return View(deger);
+        }
+
+        [HttpPost]
+        public ActionResult YorumGuncelle(Yorumlar yorumlar)
+        {
+            var value = c.Yorumlars.Find(yorumlar.ID);
+            value.KullaniciAdi = yorumlar.KullaniciAdi;
+            value.Yorum = yorumlar.Yorum;
+            value.Mail = yorumlar.Mail;
+            value.Blog.Baslik = yorumlar.Blog.Baslik;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi", "Admin");
+        }
+
+
+
     }
 }
